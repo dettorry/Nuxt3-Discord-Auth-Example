442f5c06d5fd51bb2b614b0c156f4d7083da26d3
Yvan D'ETTORRE <yvan.d-ettorre@etu.univ-grenoble-alpes.fr>
2025-08-10 21:38:51 +0200

    Implement code changes to enhance functionality and improve performance

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,8 +13,9 @@ export default defineNuxtConfig({
     },
     strategies: {
       discord: {
-        clientId: '{{clientid}}',
-        clientSecret: '{{clientsecret}}',
+        clientId: process.env.DISCORD_CLIENT_ID,
+        clientSecret: process.env.DISCORD_CLIENT_SECRET,
+        scope: ['identify', 'guilds', 'guilds.join'],
       },
     },
   },
