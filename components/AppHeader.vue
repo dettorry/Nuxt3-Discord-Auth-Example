98dba837593cd3fb3fa2539320e2c5c4d9869b3d
Yvan D'ETTORRE <yvan.d-ettorre@etu.univ-grenoble-alpes.fr>
2025-08-13 00:18:30 +0200

    Make AppHeader sticky for improved navigation experience

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="h-16 bg-white border-b border-gray-200 px-6 py-4">
+  <header class="sticky top-0 z-50 h-16 bg-white border-b border-gray-200 px-6 py-4">
     <div class="flex items-center justify-between">
       <div class="flex items-center space-x-4">
         <!-- Sidebar Toggle -->
